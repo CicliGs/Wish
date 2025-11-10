5177741bb746f9f8d746bbb563fab0514aea7ace
CicliGs <53313998+CicliGs@users.noreply.github.com>
2025-11-10 22:42:41 +0300

    delete error

--- laravel-app/bootstrap/app.php
+++ laravel-app/bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-        $middleware->trustHosts(at: ['*']);
         
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
